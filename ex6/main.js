b3ad4d1d96585e78106adfb5dd9fc39605248d20
laureschaeffer <laure.schaeffer@hotmail.com>
2024-03-16 20:38:37 +0100

    ajout exercice morpion

---
+++ ex6/main.js
@@ -0,0 +1,44 @@
+// tableau des constantes des 9 carrés et de l'espace qui affichera à qui est le tour
+const carres = document.querySelectorAll('.square');
+const msgTour = document.querySelector('.tour');
+
+
+// pour chaque carré, ajouter un ecouteur d'évènement et la fonction play en fonction de (item)
+carres.forEach(item => {
+    item.addEventListener('click', function(){
+        play(item);
+    })
+    //par défaut l'état du carré est "faux"
+    item.statue=false;
+})
+
+var countClicker=0; //compte le tour du jeu
+var tourJoueur="X"; //element du premier joueur
+msgTour.innerHTML="C'est le tour de "+tourJoueur; //affiche à qui est le tour
+
+//en fonction du carré qui a été cliqué, ajoute l'élément html
+function play(square) {
+    //si l'état du carré n'a pas été changé
+    if(square.statue==false){
+    
+        //si le tour est impair ou pair
+        if (countClicker %2==1){
+            var element="O";
+            var tourJoueur="X";
+        } else if (countClicker % 2==0){
+            var element="X";
+            var tourJoueur="O";
+        }  
+
+        square.innerHTML=element; //affiche l'element sur le carré
+        countClicker++; //compte le tour
+        msgTour.innerHTML="C'est le tour de "+tourJoueur; //affiche à qui est le tour
+
+        //je change le statut du carré pour qu'il ne puisse plus etre modifié
+        square.statue= true;
+    } 
+
+    if(countClicker == 9){
+        msgTour.innerHTML="Partie finie!"
+    }
+} 
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+//------------------------------------notes
+
+// -	Elément.addEventListener(arg1, arg2) : à chaque événement (par exemple click) une fonction est appelée, en deuxième argument
+// -	élément.cloneNode(bool) : renvoie le clone de l’élément : "If true, then the node and its whole subtree, including text that may be in child Text nodes, is also copied. If false, only the node will be cloned. The subtree, including any text that the node contains, is not cloned."
+// -	Element.value : attribue une valeur à cet élément 
+// -	Element1.appendChild(élément2) : en lien avec node, ajoute l’élément 2 à l’élément 1(le container)
+// -	Endroit.innerHTML= changement : modifie la valeur de « endroit » (dans html) par « changement »
+// -	quareStyle = window.getComputedStyle(square)
+// sortir les infos css de square
+// -	squareStyle.getPropertyValue("background-color")
+// choisir l’info qu’on veut
